a5084c6ecaa3d931ebe63b9c94425d045ea68ed8
joaopfs97 <joaopfs201@gmail.com>
2025-08-14 17:38:40 -0300

    Update imc_calculator.py

--- imc_calculator.py
+++ imc_calculator.py
@@ -3,18 +3,29 @@ weight = 0
 height = 0
 
 def retrieve_personal_data():
+    '''
+    This function is responsible to retrieve the weight(kg) and height(m) of the user
+    '''
     global weight, height
 
     try:
-        weight = float(input('Type your weight: '))
-        height = float(input('Type your height: '))
+        weight = float(input('Type your weight (kg): '))
+        height = float(input('Type your height (m): '))
     except Exception as e:
         print(f'An error occurred: {e}')
 
 def calculate_imc(weight, height):
+    '''
+    This function is responsible to calculate the IMC with the informed data\n
+    inputs: float - weight, float height\n
+    outputs: float - imc value
+    '''
     return weight / math.pow(height,2)
 
 def return_classification(imc):
+    '''
+    This function is responsible to classify the user according to the imc value
+    '''
     try:
         if(imc < 18.5):
             print('You are underweight')
